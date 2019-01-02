62fbbd66239005909c02c181106d33f593ad260a
rulin-jone <35483935+rulin-jone@users.noreply.github.com>
2019-01-02 23:37:31 +0800

    Add files via upload

--- pages/start/start.js
+++ pages/start/start.js
@@ -1,1 +1,85 @@
+// pages/start/start.js
+Page({
 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bannerUrl: [
+      '../../images/1.jpg',
+      '../../images/2.jpg',
+      '../../images/3.jpg'
+    ],
+    indicatordots: true,
+    autoplay: true,
+    interval: 4000,
+    duration: 1000,
+
+  },
+
+ 
+  //跳转到菜单页面
+  gohome:function(){
+    wx.navigateTo({
+      url: '../home/home',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '老张小吃'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
