c4c482e1b20cf2123ca1486fc05bcd58dedc3467
rulin-jone <35483935+rulin-jone@users.noreply.github.com>
2019-01-04 20:59:27 +0800

    Add files via upload

--- pages/confirm/confirm.js
+++ pages/confirm/confirm.js
@@ -6,7 +6,11 @@ Page({
    */
   data: {
     tableNumbers:0,
-    tables: ['1号', '2号', '3号', '4号', '5号', '6号', '7号', '8号', '9号', '10号', '11号','12号']
+    tables: ['1号', '2号', '3号', '4号', '5号', '6号', '7号', '8号', '9号', '10号', '11号','12号'],
+
+    cartList:[],
+    sumMoney:0,
+    orderNumber:0
   },
 
 /*因为没有微信支付的接口，所以调用摄像头，实现扫码支付*/
@@ -15,7 +19,31 @@ Page({
       success(res) {
         console.log(res)
       }
+   /* wx.requestPayment({
+        timeStamp: '',
+        nonceStr: '',
+        package: '',
+        signType: 'MD5',
+        paySign: '',
+        success(res) { },
+        fail(res) { }
+      })*/
    })
+    //wx:if = '{{success(res)}}'
+    /*wx.request({
+      url: 'https://easy-mock.com/mock/5c2e23e17096eb383e0cc254/laozhangfood/post#!method=post',
+      method: 'Post',
+      data: {
+        name:cartList.name,
+        price:cartList.sum,
+        number:cartList.number
+      },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+      }
+    })*/
   },
 
 /*选择桌号*/
@@ -30,6 +58,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '快点付款吧！'
+    })
+    this.setData({
+      cartList: wx.getStorageSync('cartList'),
+      sumMoney: wx.getStorageSync('sumMoney'),
+      orderNumber: wx.getStorageSync('orderNumber'),
+    })
 
   },
 
